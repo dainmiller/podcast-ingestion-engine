1a0094646df1ab6eae7db37b4a1ebc6d0c15af33
Dain Miller <miller.dain@gmail.com>
2019-05-24 12:32:06 -0700

    Spec'ng out some basic routes
    - Content API + User API
    - Basic shows nested resources
      - Episodes
      - Assets (photos)
    - Basic episodes nested resources
      - Assets
      - Media (urls)
    - Basic user nested resources
      - Playstate
      - Lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'shows#index', as: :json
+
+  namespace :content,
+    defaults: { format: 'json' },
+    constraints: { subdomain: 'content' } do
+
+    namespace :v1, path: '/' do
+      resources :networks
+      resources :shows do
+        resources :episodes do
+          resources :assets
+          resources :media
+        end
+        resources :assets
+      end
+    end
+  end
+
+  namespace :user,
+    defaults: { format: 'json' },
+    constraints: { subdomain: 'user' } do
+
+    namespace :v1, path: '/' do
+      resources :users do
+        resources :playstate
+        resources :lists
+      end
+    end
+  end
 end
